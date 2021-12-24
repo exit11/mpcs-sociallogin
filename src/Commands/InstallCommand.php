062ce076803bade306ebe58f90fce36dd0cecf15
hwajun Lee <hj-lee@crestec.co.kr>
2021-12-24 16:38:36 +0900

    command install

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -14,14 +14,14 @@ class InstallCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'mpcs-SocialLogin:install';
+    protected $signature = 'mpcs-socialLogin:install';
 
     /**
      * 커맨드 설명
      *
      * @var string
      */
-    protected $description = 'Install the mpcs-SocialLogin : vendor publish, migrate, seed';
+    protected $description = 'Install the mpcs-socialLogin : vendor publish, migrate, seed';
 
     protected $isForce = false;
     protected $isReset = false;
@@ -36,32 +36,12 @@ class InstallCommand extends Command
         $this->isForce = true;
         $this->isReset = true;
 
-        $confirmMessage = 'Core를 설치하시겠습니까? DB가 초기화되어, 유실될 수 있습니다. 설치를 계속 진행하시겠습니까?';
+        $confirmMessage = 'SocialLogin를 설치하시겠습니까? DB가 초기화되어, 유실될 수 있습니다. 설치를 계속 진행하시겠습니까?';
         $confirmMessage .= PHP_EOL . '(리소스는 덮어쓰기 됩니다.)';
 
         if (!app()->environment(['production'])) {
             if ($this->confirm($confirmMessage)) {
-
-                // cviebrock/eloquent-sluggable publish
-                // $this->call('vendor:publish', [
-                //     '--provider' => 'Cviebrock\EloquentSluggable\ServiceProvider',
-                //     '--tag' => 'config',
-                //     '--force' => $this->isForce
-                // ]);
-
-                // $this->line('<info>Publish cviebrock/eloquent-sluggable package configuration file:</info> config/sluggable.php');
-
-                // // cviebrock/eloquent-taggable publish
-                // $this->call('vendor:publish', [
-                //     '--provider' => 'Cviebrock\EloquentTaggable\ServiceProvider',
-                //     '--tag' => 'config',
-                //     '--force' => $this->isForce
-                // ]);
-
-                // $this->line('<info>Publish cviebrock/eloquent-taggable package configuration file:</info> config/taggable.php');
-
-                // $this->call('cache:clear');
-                // $this->call('config:cache');
+                // 
             }
         } else {
             $this->error('운영환경에서는 실행할 수 없습니다.');
